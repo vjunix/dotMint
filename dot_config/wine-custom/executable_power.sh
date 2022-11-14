10f5635e4b8c90fe00129b0b5362ac31d615a6d1
vjunix <vjunix@proton.me>
2022-11-15 01:03:53 +0530

    initial commit

---
+++ dot_config/wine-custom/executable_power.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+WINEPREFIX=/mnt/9fac1bce-85ed-435c-ae36-aec3a7ff0a61/Documents/Bottels/Windows-Apps wine /mnt/9fac1bce-85ed-435c-ae36-aec3a7ff0a61/Documents/Bottels/Windows-Apps/drive_c/Program\ Files\ \(x86\)/Microsoft\ Office/Office12/POWERPNT.EXE
